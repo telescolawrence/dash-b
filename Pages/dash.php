91b1e07cbbbcd6d0c0452018e64b378948ae7506
telesco <telescolawrence@gmail.com>
2023-09-12 15:35:47 +0300

    change

---
+++ Pages/dash.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dash.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <img src="afri1.jpeg" alt="loading">
+    </div>
+
+<div class="body">
+
+ 
+    <div class="right" >
+        <div class="right-top">
+
+        </div>
+
+        <div class="right-middle" >
+            <div class="right-middle-one">
+
+
+            </div>
+            <div class="right-middle-two" >
+
+
+            </div>
+            <div  class="right-middle-three">
+
+
+            </div>
+        </div>
+
+        <div class="right-bottom">
+
+        </div>
+
+    </div>
+
+    <div class="middle">
+        <div class="middle-top" >
+<h3>Leaderboard</h3>
+        </div>
+
+        <div class="middle-bottom">
+
+        </div>
+    </div >
+    <div class="left" >
+        <div class="left-top">
+
+        </div>
+        <div class="left-middle">
+
+        </div>
+        <div class="left-bottom" >
+            <div class="left-bottom-one">
+                <h4>Elementary School</h4>
+
+            </div>
+            <div class="left-bottom-two" >
+                <h4>Middle School</h4>
+
+            </div>
+            <div  class="left-bottom-three">
+                <h4>High School</h4>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
